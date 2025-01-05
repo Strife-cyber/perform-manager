5424caa4eee1702fde9b11415005a78678fd253e
Djiatsa Dunamis Junior <dunamisjunior15@gmail.com>
2025-01-05 11:59:55 +0100

    Added optional databases posgre or sqlite.

--- config/database.js
+++ config/database.js
@@ -1,9 +1,16 @@
 import { Sequelize } from "sequelize"
 
-const sequelize = new Sequelize('perform', 'postgres', 'admin', {
+/*const sequelize = new Sequelize('perform', 'postgres', 'admin', {
     host: 'localhost',
     dialect: 'postgres',
     logging: false
-})
+})*/ // Uncomment this to use posgresql
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './perform.sqlite',
+    logging: false
+}); // comment this if you are using posgresql
+
 
 export default sequelize;
